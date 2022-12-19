68b5f53676da61722cc73cdb32a74c75ffac8cf6
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2022-12-19 21:35:57 +0700

    Add files via upload

---
+++ src/components/Adminpage/AdminNavibar.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="/Adminorderpage"
+      style="margin-left: 30px;"
+      >
+        <img src="https://www.seekpng.com/png/detail/46-460324_microphone-clipart-talk-show-microphone-logo.png" alt="Bootstrap" width="40" height="40" />
+        B&R Karaoke Admin
+    </b-navbar-brand>
+  
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+  
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/Adminorderpage">Order</b-nav-item>
+          <b-nav-item href="/AdminPaymentpage">Payment</b-nav-item>
+          <b-nav-item href="#">Food/Drink</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Homepage/Timedate.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="margin-top: 20px;">
+        <button  @click="$emit('timereserve', time)"
+        style="width: 570px;"
+        >
+            <select v-model="time"
+            style="width: 550px;"
+            >
+                <option disabled value="">Please select one</option>
+                <option>11.00-12.00</option>
+                <option>12.00-13.00</option>
+                <option>13.00-14.00</option>
+                <option>14.00-15.00</option>
+                <option>15.00-16.00</option>
+                <option>16.00-17.00</option>
+                <option>17.00-18.00</option>
+                <option>18.00-19.00</option>
+                <option>19.00-20.00</option>
+                <option>20.00-21.00</option>
+                <option>21.00-22.00</option>
+            </select>
+    </button>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+         time:''
+        }
+      },
+    }
+  </script>
