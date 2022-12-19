2da628e661d52b5a683fd2343d17f5826a636578
Hotelcombiend <71114488+Hotelcombiend@users.noreply.github.com>
2022-12-19 21:34:30 +0700

    Delete src directory

--- src/service/Userservices.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function createOrder(data) {
-  const response = await fetch(`http://localhost:3003/addOrder`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-}
